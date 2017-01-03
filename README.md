96cb2a1e996db26bdce5674cf5d34995b32f7cb0
ocrenaka <ocrenaka@gmail.com>
2017-01-03 21:39:10 +0700

    Create console app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# makepingtoservies
+# Description
+A small app makes a fake request to Auzre app services (Basic, Shared) to prevent idle
+
+# Example
+- Update the url you want to ping in code
+```sh
+const string _url = "{your_url}";
+```
+
+- Make a request with command line
+```sh 
+SH.MakePing.App.exe
+```
---
+++ src/SH.MakePing.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace SH.MakePing.App
+{
+    class Program
+    {
+        const string _url = "{your_url}";
+        static HttpClient _httpClient = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            var response = _httpClient.GetAsync(_url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
